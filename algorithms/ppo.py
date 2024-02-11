3852ecb2817a76e4d60888cca72271b5cc3ba6ba
ZagainovNikita <nikitazag3@gmail.com>
2024-02-11 17:00:07 +0300

    add PPO algorithm

---
+++ algorithms/ppo.py
@@ -0,0 +1,107 @@
+import torch
+from torch.nn import functional as F
+from torch.distributions import Categorical
+import numpy as np
+
+
+class PPO:
+    def __init__(self, env, actor, critic,
+                 clip=0.1, gamma=0.95):
+        self.env = env
+        self.actor = actor
+        self.critic = critic
+        
+        self.gamma = gamma
+        self.clip = clip
+    
+    def learn(self, n_epochs=10, 
+              timesteps_per_epoch=1000, updates_per_epoch=5, 
+              batch_size=64):
+        for epoch in range(n_epochs):
+            states, actions, probs, rewards, dones, batches = \
+                self.collect_rollouts(timesteps=timesteps_per_epoch, batch_size=batch_size)
+                
+            mean_reward = torch.sum(rewards) / torch.sum(dones.to(torch.int))
+            print(mean_reward.item())
+                
+            rwtg = torch.zeros_like(rewards, dtype=torch.float32)
+            rew_to_go = 0
+            for t in reversed(range(len(rewards))):
+                rew_to_go = rewards[t] + 0.95 * rew_to_go * (1 - int(dones[t]))
+                rwtg[t] = rew_to_go
+                
+            for update in range(updates_per_epoch):
+                for batch in batches:                    
+                    states_batch = states[batch].clone().to(self.actor.device)
+                    actions_batch = actions[batch].clone().to(self.actor.device)
+                    old_probs_batch = probs[batch].clone().to(self.actor.device)
+                    rwtg_batch = rwtg[batch].clone().to(self.actor.device)
+                    
+                    new_dist = self.actor.get_distribution(states_batch)
+                    new_probs_batch = new_dist.log_prob(actions_batch)
+                    values_batch = self.critic(states_batch)
+                    
+                    advandages_batch = rwtg_batch - values_batch
+                    ratios = torch.exp(new_probs_batch - old_probs_batch)
+                    
+                    surrogate_loss1 = ratios * advandages_batch
+                    surrogate_loss2 = torch.clip(ratios * advandages_batch, 
+                                                 1 - self.clip, 1 + self.clip)
+                    
+                    actor_loss = -torch.min(surrogate_loss1, surrogate_loss2).mean()
+                    critic_loss = F.mse_loss(values_batch, rwtg_batch.view(values_batch.shape))
+                    total_loss = actor_loss + critic_loss
+                    
+                    total_loss.backward()
+                    
+                    self.actor.optimizer.step()
+                    self.critic.optimizer.step()
+
+                    self.actor.optimizer.zero_grad()
+                    self.critic.optimizer.zero_grad()
+            
+            
+
+    def collect_rollouts(self, timesteps, batch_size):
+        states = []
+        actions = []
+        probs = []
+        rewards = []
+        dones = []
+        
+        cur_t = 0
+        while cur_t < timesteps:
+            done = False
+            state, _ = self.env.reset()
+            while not done:
+                states.append(state)
+                action, prob = self.get_action(state)
+                actions.append(action)
+                probs.append(prob)
+                
+                state, reward, done, _, _ = self.env.step(action)
+                dones.append(done)
+                rewards.append(reward)
+                cur_t += 1
+            
+        indices = torch.randperm(len(states))[:timesteps]
+        batches = [indices[start_idx:start_idx+batch_size] 
+                   for start_idx in range(0, timesteps, batch_size)]
+        
+        return (torch.tensor(np.array(states), dtype=torch.float32),
+                torch.tensor(actions),
+                torch.tensor(probs, dtype=torch.float32),
+                torch.tensor(rewards, dtype=torch.float32),
+                torch.tensor(dones),
+                batches)
+            
+    
+    def get_action(self, state):
+        with torch.no_grad():
+            dist = self.actor.get_distribution(
+                torch.tensor(state, device=self.actor.device))
+
+            action = dist.sample()
+            prob = dist.log_prob(action)
+            
+        return action.item(), prob.item()
--- cart_pole.py
+++ cart_pole.py
@@ -1,5 +1,5 @@
 import gymnasium as gym
-from algorithms.ppo.gae import PPO
+from algorithms.ppo import PPO
 from models.feed_forward import FeedForwardNN
 
 
@@ -7,6 +7,6 @@ env = gym.make("CartPole-v1")
 actor = FeedForwardNN(env.observation_space.shape[0], env.action_space.n, lr=1e-4)
 critic = FeedForwardNN(env.observation_space.shape[0], 1, lr=1e-4)
 
-ppo = PPO(env, actor, critic, batch_size=64)
+ppo = PPO(env, actor, critic)
 
-ppo.learn(30, 2500)
+ppo.learn(30, 1000, 3)
